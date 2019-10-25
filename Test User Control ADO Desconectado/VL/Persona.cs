bba5d6fb926ced50fb58a737364cb42db0f3ab48
navegador <navegador@342-22-84561>
2019-10-25 09:48:29 -0300

    Added Persona Class

---
+++ Test User Control ADO Desconectado/VL/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL
+{
+    public class Persona
+    {
+        public int Id { get; private set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public Persona(int Id, string Nombre, string Apellido)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+        }
+    }
+}
