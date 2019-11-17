bbad85e9af1f3b9b4f9d0b72d5d474ee42e026f3
IanBuceta <iansbrizza@gmail.com>
2019-11-17 15:19:24 -0300

    Changed implementation of "Consulta" Method

--- Test 6 Capas/Interfaces/IABMC.cs
+++ Test 6 Capas/Interfaces/IABMC.cs
@@ -18,8 +18,8 @@ namespace Interfaces
     {
         void Alta(T pObject);
         void Baja(T pObject);
-        void Modificacion(T pObject);
-        void Consulta(T pObject);
+        void Modificacion(T pObject, int IdViejo);
+        T Consulta(T pObject);
         List<T> ConsultaTodos();
     }
 }
